c044b2e3942c5f57723fd17d42d1bb5f93c9d80a
Daniil Pankieiev <daniil.pankieiev@gmail.com>
2023-11-30 10:56:14 +0200

    Solution

--- movies/serializers.py
+++ movies/serializers.py
@@ -3,20 +3,16 @@ from rest_framework import serializers
 from movies.models import Movie, Director, Star, Genre, Certification
 
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=250)
-    year = serializers.IntegerField()
-    time = serializers.IntegerField()
-    imdb = serializers.FloatField()
-    votes = serializers.IntegerField()
-    meta_score = serializers.FloatField(allow_null=True)
-    gross = serializers.FloatField(allow_null=True)
-    certification = serializers.PrimaryKeyRelatedField(queryset=Certification.objects.all())
-    description = serializers.CharField()
+class MovieSerializer(serializers.ModelSerializer):
+
     genre_ids = serializers.PrimaryKeyRelatedField(queryset=Genre.objects.all(), many=True, required=False)
     director_ids = serializers.PrimaryKeyRelatedField(queryset=Director.objects.all(), many=True, required=False)
     star_ids = serializers.PrimaryKeyRelatedField(queryset=Star.objects.all(), many=True, required=False)
+
+    class Meta:
+        model = Movie
+        fields = ('id', 'name', 'year', 'time', 'imdb', 'votes', 'meta_score', 'gross', 'certification',
+                  'description', 'genre_ids', 'director_ids', 'star_ids')
 
     def create(self, validated_data):
         genre_ids = validated_data.pop('genre_ids', [])
